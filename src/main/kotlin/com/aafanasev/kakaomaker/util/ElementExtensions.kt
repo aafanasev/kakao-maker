521ad14189c6d34ce2cd527c4193f345880464fc
aafanasev <tafanasyev@gmail.com>
2019-01-09 23:07:29 +0700

    add element extensions function to easily read XML

---
+++ src/main/kotlin/com/aafanasev/kakaomaker/util/ElementExtensions.kt
@@ -0,0 +1,28 @@
+package com.aafanasev.kakaomaker.util
+
+import org.w3c.dom.Element
+
+internal const val ATTR_ANDROID_NAMESPACE = "http://schemas.android.com/apk/res/android"
+internal const val ATTR_KAKAO_NAMESPACE = "http://schemas.android.com/tools"
+internal const val ATTR_ID = "id"
+internal const val ATTR_SCREEN = "kakaoScreen"
+internal const val ATTR_TYPE = "kakaoType"
+internal const val ATTR_IGNORE = "kakaoIgnore"
+
+internal val Element.id: String
+    get() = getAttributeNS(ATTR_ANDROID_NAMESPACE, ATTR_ID).substringAfterLast("/")
+
+internal val Element.kakaoScreenName: String
+    get() = getAttributeNS(ATTR_KAKAO_NAMESPACE, ATTR_SCREEN)
+
+internal val Element.kakaoType: String
+    get() = getAttributeNS(ATTR_ANDROID_NAMESPACE, ATTR_TYPE)
+
+internal val Element.kakaoIgnore: Boolean
+    get() = getAttributeNS(ATTR_ANDROID_NAMESPACE, ATTR_IGNORE).isNotEmpty()
+
+internal val Element.isMergeTag: Boolean
+    get() = tagName == "merge"
+
+internal val Element.isIncludeTag: Boolean
+    get() = tagName == "include"
